98151075545f2fd616b365875339cdfefc950e3f
Mhmdhusseini <mhmdcs65@gmail.com>
2025-03-18 20:46:08 +0200

    Add files via upload

---
+++ megaa/megaa/Program.cs
@@ -0,0 +1,23 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter birthdate like this (yyyy-mm-dd) ");
+        if (DateTime.TryParse(Console.ReadLine(), out DateTime birthDate))
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - birthDate.Year;
+
+            if (birthDate.Date > today.AddYears(age))
+                age--;
+
+            Console.WriteLine($"Your age is  {age}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid date format");
+        }
+        Console.ReadKey();
+    }
+}
