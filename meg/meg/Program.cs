9d78f362da5e9f3e6c1fa2efe9931180b0162c6d
Mhmdhusseini <mhmdcs65@gmail.com>
2025-03-19 02:09:12 +0200

    Add files via upload

---
+++ meg/meg/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the length of the password: ");
+        if (int.TryParse(Console.ReadLine(), out int length) && length > 0)
+        {
+            string password = GeneratePassword(length);
+            Console.WriteLine($"Generated Password: {password}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid num, enter positive number ");
+        }
+        Console.ReadKey();
+    }
+
+    static string GeneratePassword(int length)
+    {
+        const string digits = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder password = new StringBuilder();
+        Random random = new Random();
+
+        for (int i = 0; i < length; i++)
+        {
+            password.Append(digits[random.Next(digits.Length)]);
+        }
+
+        return password.ToString();
+    }
+}
+
